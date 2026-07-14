548f2b21d67cf465c43f04ba5c5351d86cf2fbeb
RobertoOteri <rob.oteri@stud.uniroma3.it>
2026-07-14 19:07:46 +0200

    Aggiunto primo movimento per il player

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+//using System.Numerics;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5;
+    public Rigidbody2D rb;
+ 
+
+    // FixedUpdate is called 50 per frame
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector2(horizontal, vertical) * speed;
+    }
+}
