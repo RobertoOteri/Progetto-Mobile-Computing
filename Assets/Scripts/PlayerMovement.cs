92bb375ca31d655ae4c45a04282246457f32980a
RobertoOteri <rob.oteri@stud.uniroma3.it>
2026-07-15 09:26:28 +0200

    Aggiunto movimento e animazioni di movimento per il player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,10 +1,15 @@
 //using System.Numerics;
+//using System.Numerics;
+//using System.Threading.Tasks.Dataflow;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5;
+    public int facingDirection = 1;
     public Rigidbody2D rb;
+
+    public Animator anim;
  
 
     // FixedUpdate is called 50 per frame
@@ -13,6 +18,29 @@ public class PlayerMovement : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
+        if((horizontal > 0 && transform.localScale.x > 0) ||
+           (horizontal < 0 && transform.localScale.x < 0))
+        {
+            Flip();
+        }
+
+        if (horizontal != 0)
+        {
+            anim.SetFloat("horizontal", horizontal);
+            anim.SetFloat("vertical", 0);
+        }
+        else
+        {
+            anim.SetFloat("horizontal", horizontal);
+            anim.SetFloat("vertical", vertical);
+        }
+
         rb.velocity = new Vector2(horizontal, vertical) * speed;
+    }
+
+    void Flip()
+    {
+        facingDirection *= -1;
+        transform.localScale = new Vector3 (transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
     }
 }
