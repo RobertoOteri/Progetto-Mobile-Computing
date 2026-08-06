abd84596aa5643988ccbd94e6f6a4a203aba857c
AntCoco2 <antoniococo690@gmail.com>
2026-08-06 23:06:34 +0200

    Aggiunto menu' impostazioni
    Aggiunto menu' impostazioni. Lo devo finire e metto qualcosa per le animazioni

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -3,16 +3,36 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
+    [Header("Pannelli del Menù")]
+    public GameObject pannelloPrincipale;
+    public GameObject pannelloImpostazioni;
+
     public void Gioca()
     {
-        // Carica la scena successiva in coda (il tuo gioco)
+        // Carica il primo livello del gioco
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void Esci()
     {
-        // Chiude l'applicazione (funziona solo nel gioco esportato, non nell'editor)
+        // Chiude l'applicazione
         Debug.Log("Uscita dal gioco!");
         Application.Quit();
+    }
+
+    // --- NUOVE FUNZIONI PER LE IMPOSTAZIONI ---
+
+    public void ApriImpostazioni()
+    {
+        // Nasconde il menù principale e mostra quello delle impostazioni
+        pannelloPrincipale.SetActive(false);
+        pannelloImpostazioni.SetActive(true);
+    }
+
+    public void ChiudiImpostazioni()
+    {
+        // Nasconde il menù delle impostazioni e torna al principale
+        pannelloImpostazioni.SetActive(false);
+        pannelloPrincipale.SetActive(true);
     }
 }
