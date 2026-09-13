9d2dfb88960ba2b6e6341810ffdf360a7fae3f97
RobertoOteri <rob.oteri@stud.uniroma3.it>
2026-09-13 12:23:17 +0200

    Creata build per ios

---
+++ ios-build/Il2CppOutputProject/Source/il2cppOutput/Il2CppMetadataRegistration.c
@@ -0,0 +1,36 @@
+﻿#include "pch-c.h"
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern Il2CppGenericClass* const g_Il2CppGenericTypes[];
+extern const Il2CppGenericInst* const g_Il2CppGenericInstTable[];
+extern const Il2CppGenericMethodFunctionsDefinitions g_Il2CppGenericMethodFunctions[];
+extern const Il2CppType* const  g_Il2CppTypeTable[];
+extern const Il2CppMethodSpec g_Il2CppMethodSpecTable[];
+extern const int32_t* g_FieldOffsetTable[];
+extern const Il2CppTypeDefinitionSizes* g_Il2CppTypeDefinitionSizesTable[];
+IL2CPP_EXTERN_C const Il2CppMetadataRegistration g_MetadataRegistration;
+const Il2CppMetadataRegistration g_MetadataRegistration = 
+{
+	6499,
+	g_Il2CppGenericTypes,
+	4359,
+	g_Il2CppGenericInstTable,
+	64929,
+	g_Il2CppGenericMethodFunctions,
+	20026,
+	g_Il2CppTypeTable,
+	74372,
+	g_Il2CppMethodSpecTable,
+	6092,
+	g_FieldOffsetTable,
+	6092,
+	g_Il2CppTypeDefinitionSizesTable,
+	0,
+	NULL,
+};
---
+++ ios-build/Il2CppOutputProject/Source/il2cppOutput/Unity.InternalAPIEngineBridge.001_CodeGen.c
@@ -0,0 +1,51 @@
+﻿#include "pch-c.h"
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern void InternalEngineBridge_SetLocalAABB_m04DF27869CF8971148682477CFEC2A1991BB104E (void);
+extern void InternalEngineBridge_SetBatchDeformableBufferAndLocalAABBArray_mE474FF62DD89223FCCC68A8DFFBA61935B3229A3 (void);
+extern void InternalEngineBridge_SetBatchBoneTransformIndexAndLocalAABBArray_mF1C2297CDF4675A5E2AFB7B502042133D55C9E41 (void);
+extern void InternalEngineBridge_ConvertFloatToInt_mE74F74479983A239EE750CAF78760F79F6717F44 (void);
+extern void InternalEngineBridge_ConvertIntToFloat_mB38ED35A3CD802442791415694C3E8C7E86E8148 (void);
+static Il2CppMethodPointer s_methodPointers[5] = 
+{
+	InternalEngineBridge_SetLocalAABB_m04DF27869CF8971148682477CFEC2A1991BB104E,
+	InternalEngineBridge_SetBatchDeformableBufferAndLocalAABBArray_mE474FF62DD89223FCCC68A8DFFBA61935B3229A3,
+	InternalEngineBridge_SetBatchBoneTransformIndexAndLocalAABBArray_mF1C2297CDF4675A5E2AFB7B502042133D55C9E41,
+	InternalEngineBridge_ConvertFloatToInt_mE74F74479983A239EE750CAF78760F79F6717F44,
+	InternalEngineBridge_ConvertIntToFloat_mB38ED35A3CD802442791415694C3E8C7E86E8148,
+};
+static const int32_t s_InvokerIndices[5] = 
+{
+	9387,
+	8271,
+	8721,
+	9781,
+	9894,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_Unity_InternalAPIEngineBridge_001_CodeGenModule;
+const Il2CppCodeGenModule g_Unity_InternalAPIEngineBridge_001_CodeGenModule = 
+{
+	"Unity.InternalAPIEngineBridge.001.dll",
+	5,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+};
